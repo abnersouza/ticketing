f6e69ebd8547ffcf0aac05a521937c4d4d867627
Abner Souza <abnersouza@outlook.com>
2020-08-19 14:19:02 -0300

    Adding console to the services startup

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,6 +5,8 @@ import { OrderCreatedListener } from "./events/listeners/order-created-listener"
 import { OrderCancelledListener } from "./events/listeners/order-cancelled-listener";
 
 const start = async () => {
+  console.log("Starting Tickets Service");
+
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY env must be defined.");
   }
