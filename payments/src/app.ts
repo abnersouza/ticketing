bf4578b0c88baac7ed7d78e1289bc89eb320ef89
Abner Souza <abnersouza@outlook.com>
2020-08-19 18:07:05 -0300

    Disabling secure to token

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -11,7 +11,8 @@ app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== "test",
+    // secure: process.env.NODE_ENV !== "test",
+    secure: false,
   })
 );
 
