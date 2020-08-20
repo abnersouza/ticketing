6ad8223c53b1cb173db26267e0dc8b2248afc886
Abner Souza <abnersouza@outlook.com>
2020-08-20 17:13:16 -0300

    Making all the services and client to run in prod mode

--- client/components/header.js
+++ client/components/header.js
@@ -22,7 +22,7 @@ const Header = ({ currentUser }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <Link href="/">
-        <a className="navbar-brand">GitTix</a>
+        <a className="navbar-brand">ABRTICKETS</a>
       </Link>
 
       <div className="d-flex justify-content-end">
